3408c2dcd05a189da9c0f160918765b6dd6f2682
Kenny Tran <kenny.tran138@gmail.com>
2021-02-09 19:30:30 -0500

    completed exponent

--- src/recursion.js
+++ src/recursion.js
@@ -89,6 +89,18 @@ var range = function (x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function (base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp > 0 && exp % 2 === 0) {
+    return exponent(base, exp / 2) * exponent(base, exp / 2);
+  }
+  if (exp > 0 && exp % 2 === 1) {
+    return base * exponent(base, exp - 1);
+  }
+  if (exp < 0) {
+    return 1 / exponent(base, exp * -1);
+  }
 };
 
 // 8. Determine if a number is a power of two.
